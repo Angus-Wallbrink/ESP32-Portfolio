140fcdb106347ba179f6377788453a5dfd91b599
Angus-Wallbrink <141972082+Angus-Wallbrink@users.noreply.github.com>
2023-08-17 12:51:43 +1000

    Update Analog_Reader.ino

--- Analog Reader/Analog_Reader.ino
+++ Analog Reader/Analog_Reader.ino
@@ -1,16 +1,20 @@
+// Defines the pin connected to the potentiometer
 const int potPin = 4;
-
+// Variable to store the analog reading
 int potValue = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  // Initializes the serial communication at a baud rate of 9600  
   Serial.begin(9600);
+  // Sets a delay of 1000 milliseconds for initialization
   delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Reads the analog value of the potentiometer
   potValue = analogRead(potPin);
+  // Prints the value of the potentiometer to the serial moniter
   Serial.println(potValue);
+  // Sets a delay of 500 milliseconds before the next iteration of the loop
   delay(500);
 }
