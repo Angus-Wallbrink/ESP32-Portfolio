afe76a6e8535f531d43e07db70322f314afcc8f1
Angus-Wallbrink <141972082+Angus-Wallbrink@users.noreply.github.com>
2023-08-17 12:55:01 +1000

    Update Button_Rgb.ino

--- Input-Output Button/Button_Rgb.ino
+++ Input-Output Button/Button_Rgb.ino
@@ -1,11 +1,14 @@
-// set pin numbers
-const int buttonPin = 4;  // the number of the pushbutton pin
-const int ledPin =  5;    // the number of the LED pin
+// sets the pin numbers
+// the number of the pushbutton pin
+const int buttonPin = 4;  
+// the number of the LED pin
+const int ledPin =  5;    
 
 // variable for storing the pushbutton status
 int buttonState = 0;
 
 void setup() {
+  // Initializes serial communication at a baud rate of 115200 bits per second
   Serial.begin(115200);  
   // initializes the pushbutton pin as an input
   pinMode(buttonPin, INPUT);
@@ -17,8 +20,7 @@ void loop() {
   // reads the state of the pushbutton value
   buttonState = digitalRead(buttonPin);
   Serial.println(buttonState);
-  // checks if the pushbutton is pressed.
-  // if it is, the buttonState is set to HIGH
+  // checks if the pushbutton is pressed, if it is, the buttonState is set to HIGH
   if (buttonState == HIGH) {
     // turns the  LED on
     digitalWrite(ledPin, HIGH);
